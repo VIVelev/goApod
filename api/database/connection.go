251a33272dc077b6ef27d7d811f90e820b592fd1
Valentin <valentinspasov9669@gmail.com>
2020-06-08 00:39:26 +0300

    Fix: glupostite na viki

--- api/database/connection.go
+++ api/database/connection.go
@@ -13,17 +13,10 @@ var Connection *sql.DB
 
 // Connect establishing a connection to the database
 func Connect() error {
-	var (
-		host     = os.Getenv("DB_HOST")
-		port     = os.Getenv("DB_PORT")
-		user     = os.Getenv("DB_USER")
-		password = os.Getenv("DB_PASSWORD")
-		dbname   = os.Getenv("DB_NAME")
-	)
-
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
