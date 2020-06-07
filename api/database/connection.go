c3a9bd15db9f3db4a1086c52e5c2f01689bdec8c
Victor Velev <victorivelev@gmail.com>
2020-06-08 00:27:36 +0300

    fix: env variables parsing for db conn

--- api/database/connection.go
+++ api/database/connection.go
@@ -8,19 +8,19 @@ import (
 	_ "github.com/lib/pq" // postgresql
 )
 
-var (
-	host     = os.Getenv("DB_HOST")
-	port     = os.Getenv("DB_PORT")
-	user     = os.Getenv("DB_USER")
-	password = os.Getenv("DB_PASSWORD")
-	dbname   = os.Getenv("DB_NAME")
-)
-
 // Connection conn
 var Connection *sql.DB
 
 // Connect establishing a connection to the database
 func Connect() error {
+	var (
+		host     = os.Getenv("DB_HOST")
+		port     = os.Getenv("DB_PORT")
+		user     = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+		dbname   = os.Getenv("DB_NAME")
+	)
+
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
